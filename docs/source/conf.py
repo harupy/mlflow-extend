440ddefa368ddb6ef816ac79735a3956894dd304
harupy <hkawamura0130@gmail.com>
2020-02-25 01:48:52 +0900

    Comment out html_static_path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,12 +20,12 @@
 from datetime import datetime
 import sphinx_rtd_theme
 
-project = 'mlflow-extend'
-copyright = '{}, harupy'.format(datetime.utcnow().year)
-author = 'harupy'
+project = "mlflow-extend"
+copyright = "{}, harupy".format(datetime.utcnow().year)
+author = "harupy"
 
 # The full version, including alpha/beta/rc tags
-release = '[]'
+release = "[]"
 
 
 # -- General configuration ---------------------------------------------------
@@ -34,20 +34,20 @@ release = '[]'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -60,9 +60,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
