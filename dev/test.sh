1088ae48e7874cfd04eb3984d68c6cb76976b0ce
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-02-26 00:05:49 +0900

    Add doc for logging (#13)
    * Fix test options
    
    * Add doctest items
    
    * Add docs for logging
    
    * Fix doc source

--- dev/test.sh
+++ dev/test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-pytest . \
-  --cov mlflow_extend --cov-report html --verbose --showlocals \
-  --basetemp .pytest_basetemp --color=yes --durations=30 --doctest-modules mlflow_extend "$@"
+pytest tests --doctest-modules mlflow_extend --cov mlflow_extend \
+  --verbose --showlocals --color=yes --durations=30 --basetemp .pytest_basetemp "$@"
+
+
