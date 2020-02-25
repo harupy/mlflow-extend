85ff5a1b70dbc264ccae3c8309f6e391eae1d2f0
harupy <hkawamura0130@gmail.com>
2020-02-25 22:54:48 +0900

    Fix lint script

--- dev/lint.sh
+++ dev/lint.sh
@@ -2,10 +2,10 @@
 
 exit_code=0
 
-black --check .
+black --check tests mlflow_extend
 [[ $? = 1 ]] && exit_code=1
 
-flake8 .
+flake8 tests mlflow_extend
 [[ $? = 1 ]] && exit_code=1
 
 exit $exit_code
