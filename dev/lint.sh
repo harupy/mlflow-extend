a713049d03dc0fa8d5e29fd0aade2080373736e6
harupy <hkawamura0130@gmail.com>
2020-02-26 18:43:22 +0900

    Add workaround for black

--- dev/lint.sh
+++ dev/lint.sh
@@ -2,8 +2,13 @@
 
 exit_code=0
 
-black --check tests mlflow_extend
-[[ $? = 1 ]] && exit_code=1
+python_ver=$(python -c 'import sys; print("{}.{}".format(*sys.version_info[:2]))')
+
+# Black does not support python 3.5.
+if [ ! "$python_ver" == "3.5" ]; then
+  black --check tests mlflow_extend
+  [[ $? = 1 ]] && exit_code=1
+fi
 
 flake8 tests mlflow_extend
 [[ $? = 1 ]] && exit_code=1
