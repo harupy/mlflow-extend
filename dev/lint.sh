97b9b5f52398b89c6c39f1c2b6802d9c2aa6c07c
harupy <hkawamura0130@gmail.com>
2020-02-26 18:44:23 +0900

    Change update logic for exit_code

--- dev/lint.sh
+++ dev/lint.sh
@@ -7,10 +7,10 @@ python_ver=$(python -c 'import sys; print("{}.{}".format(*sys.version_info[:2]))
 # Black does not support python 3.5.
 if [ ! "$python_ver" == "3.5" ]; then
   black --check tests mlflow_extend
-  [[ $? = 1 ]] && exit_code=1
+  exit_code=$(( $? | $exit_code ))
 fi
 
 flake8 tests mlflow_extend
-[[ $? = 1 ]] && exit_code=1
+exit_code=$(( $? | $exit_code ))
 
 exit $exit_code
