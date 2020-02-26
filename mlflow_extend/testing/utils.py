0c0478cf383e046194449984910be47f16764025
harupy <hkawamura0130@gmail.com>
2020-02-27 00:43:20 +0900

    Add docstring

--- mlflow_extend/testing/utils.py
+++ mlflow_extend/testing/utils.py
@@ -5,6 +5,9 @@ import mlflow
 
 
 def get_default_args(func):
+    """
+    Get default arguments of the given function.
+    """
     return {
         k: v.default
         for k, v in inspect.signature(func).parameters.items()
@@ -13,6 +16,9 @@ def get_default_args(func):
 
 
 def run_python_script(path):
+    """
+    Run a python script and return exit code.
+    """
     child = subprocess.Popen(
         ["export PYTHONPATH=$(pwd) && python {}".format(path)], shell=True
     )
@@ -21,6 +27,9 @@ def run_python_script(path):
 
 
 def _list_artifacts(run_id, root=""):
+    """
+    List all artifacts in the specified run.
+    """
     client = mlflow.tracking.MlflowClient()
     artifacts = []
     for artifact in client.list_artifacts(run_id, None if root == "" else root):
@@ -33,9 +42,15 @@ def _list_artifacts(run_id, root=""):
 
 
 def assert_file_exists(path):
+    """
+    Assert the specified file exists.
+    """
     assert os.path.exists(path)
 
 
 def assert_file_exists_in_artifacts(run, path):
+    """
+    Assert the specified file exists in the artifact store of the given run.
+    """
     artifacts = _list_artifacts(run.info.run_id)
     assert path in artifacts
