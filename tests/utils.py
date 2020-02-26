3c3689546cd8e0b5a1c104bbaabe8b1c2f8a738a
harupy <hkawamura0130@gmail.com>
2020-02-26 22:17:14 +0900

    Add shell=True

--- tests/utils.py
+++ tests/utils.py
@@ -13,7 +13,9 @@ def get_default_args(func):
 
 
 def run_python_script(path):
-    child = subprocess.Popen(["export PYTHONPATH=$(pwd) && python {}".format(path)])
+    child = subprocess.Popen(
+        ["export PYTHONPATH=$(pwd) && python {}".format(path)], shell=True
+    )
     child.communicate()
     return child.returncode
 
