3c79be9f4f7321239f45dfeb39a6f48c2477fd87
harupy <hkawamura0130@gmail.com>
2020-02-26 18:57:31 +0900

    Add python 3.5 to classifiers

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     long_description_content_type="text/markdown",
     classifiers=[
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
