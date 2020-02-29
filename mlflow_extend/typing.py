aeedebcbc13254d09237c2979c53acbe18f72114
harupy <hkawamura0130@gmail.com>
2020-02-29 19:52:37 +0900

    Fix ArrayLike

--- mlflow_extend/typing.py
+++ mlflow_extend/typing.py
@@ -3,4 +3,4 @@ from typing import Union
 import numpy as np
 import pandas as pd
 
-ArrayLike = Union[dict, tuple, set, np.array, pd.Series, pd.DataFrame]
+ArrayLike = Union[list, tuple, set, np.array, pd.Series, pd.DataFrame]
