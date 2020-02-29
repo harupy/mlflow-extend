b6084c9830106c610e0e0c1de9bd9ebef78d1399
harupy <hkawamura0130@gmail.com>
2020-02-29 22:51:14 +0900

    Add related issue

--- mlflow_extend/mlflow.py
+++ mlflow_extend/mlflow.py
@@ -1,6 +1,7 @@
 from mlflow import *  # noqa
 
-# Some functions are not included in mlflow.__all__.
+# Some fluent APIs are not included in mlflow.__all__:
+# https://github.com/mlflow/mlflow/pull/2511
 from mlflow import (  # noqa: F401
     get_experiment_by_name,
     get_experiment,
