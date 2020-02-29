b115194f5ec86fdd26c7a1c14d087d8aae8a7619
harupy <hkawamura0130@gmail.com>
2020-02-29 23:22:18 +0900

    Update lint script

--- dev/lint.sh
+++ dev/lint.sh
@@ -6,14 +6,14 @@ python_ver=$(python -c 'import sys; print("{}.{}".format(*sys.version_info[:2]))
 
 # Black does not support python 3.5.
 if [ ! "$python_ver" == "3.5" ]; then
-  black --check tests mlflow_extend
+  black --check .
   exit_code=$(( $? | $exit_code ))
 fi
 
 isort --check-only
 exit_code=$(( $? | $exit_code ))
 
-flake8 tests mlflow_extend
+flake8 .
 exit_code=$(( $? | $exit_code ))
 
 exit $exit_code
