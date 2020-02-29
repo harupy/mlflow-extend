8ae2044f531370c4cf902814f8a49d389515eac0
harupy <hkawamura0130@gmail.com>
2020-02-29 21:57:12 +0900

    Add missing apis

--- mlflow_extend/mlflow.py
+++ mlflow_extend/mlflow.py
@@ -1,2 +1,11 @@
 from mlflow import *  # noqa
+
+# Some functions are not included in mlflow.__all__.
+from mlflow import (  # noqa: F401
+    get_experiment_by_name,
+    get_experiment,
+    get_run,
+    get_tracking_uri,
+)
+
 from mlflow_extend.logging import *  # noqa
