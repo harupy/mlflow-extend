033016ab36c191b0da5b26a2fb361c8c43025f31
harupy <hkawamura0130@gmail.com>
2020-03-01 22:31:58 +0900

    Fix lint

--- mlflow_extend/mlflow.py
+++ mlflow_extend/mlflow.py
@@ -1,13 +1,12 @@
-from mlflow import *  # noqa;
-
 # Some fluent APIs are not included in mlflow.__all__:
 # https://github.com/mlflow/mlflow/pull/2511
-from mlflow import (  # noqa
+from mlflow import *
+from mlflow import (
     get_experiment,
     get_experiment_by_name,
     get_run,
     get_tracking_uri,
 )
 
-from mlflow_extend.logging import *  # noqa
-from mlflow_extend.experiment import *  # noqa
+from mlflow_extend.experiment import *
+from mlflow_extend.logging import *
