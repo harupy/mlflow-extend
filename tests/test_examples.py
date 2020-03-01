dd41fce80352fd596081221625fc29e9149d55a3
harupy <hkawamura0130@gmail.com>
2020-03-02 01:06:52 +0900

    Add test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,6 +3,8 @@ import pytest
 from mlflow_extend.testing.utils import _run_python_script
 
 
-@pytest.mark.parametrize("path", ["examples/quickstart.py"])
+@pytest.mark.parametrize(
+    "path", ["examples/quickstart.py", "examples/lightgbm_binary.py"]
+)
 def test_examples(path):
     assert _run_python_script(path) == 0
