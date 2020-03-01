2e2ce89d53223c7e419cf954809edac918cc5c43
harupy <hkawamura0130@gmail.com>
2020-03-01 12:01:05 +0900

    Ignore isort for python 3.5

--- dev/lint.sh
+++ dev/lint.sh
@@ -4,14 +4,14 @@ exit_code=0
 
 python_ver=$(python -c 'import sys; print("{}.{}".format(*sys.version_info[:2]))')
 
-# Black does not support python 3.5.
+# Black and isort (latest version) don't support python 3.5.
 if [ ! "$python_ver" == "3.5" ]; then
   black --check .
   exit_code=$(( $? | $exit_code ))
-fi
 
-isort --check-only .
-exit_code=$(( $? | $exit_code ))
+  isort --check-only .
+  exit_code=$(( $? | $exit_code ))
+fi
 
 flake8 .
 exit_code=$(( $? | $exit_code ))
