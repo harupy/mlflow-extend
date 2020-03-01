45a1beb74a32dc6c8254fdbf55c8d2a47e92c47a
harupy <hkawamura0130@gmail.com>
2020-03-01 12:13:39 +0900

    Capitalize

--- dev/lint.sh
+++ dev/lint.sh
@@ -4,7 +4,7 @@ exit_code=0
 
 python_ver=$(python -c 'import sys; print("{}.{}".format(*sys.version_info[:2]))')
 
-# Black and isort (latest version) don't support python 3.5.
+# Black and isort (latest version) don't support Python 3.5.
 if [ ! "$python_ver" == "3.5" ]; then
   black --check .
   exit_code=$(( $? | $exit_code ))
