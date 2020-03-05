cb816b43d49b26355ca964646519c87cbcc90374
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-03-05 22:11:34 +0900

    Add action for codecov (#57)
    * Add a job for codecov
    
    * Use os as name
    
    * Use python version as name
    
    * Use both os and python-version as name
    
    * Switch report
    
    * Fix

--- dev/test.sh
+++ dev/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-pytest tests --doctest-modules mlflow_extend --cov mlflow_extend \
+# On GitHub Action, use "xml", "term" otherwise.
+report=$([ ! -z "$GITHUB_ACTION" ] && echo "xml" || echo "term")
+
+pytest tests --doctest-modules mlflow_extend --cov mlflow_extend --cov-report="$report" \
   --verbose --showlocals --color=yes --durations=30 --basetemp .pytest_basetemp "$@"
-
-
