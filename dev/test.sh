9d70360767ca6706ba15c96c7375b24da6b1f056
harupy <hkawamura0130@gmail.com>
2020-03-05 22:00:07 +0900

    Switch report

--- dev/test.sh
+++ dev/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+repot=$([ -z "$GITHUB_ACTION" ] && echo "xml" || echo "term")
+
 pytest tests --doctest-modules mlflow_extend --cov mlflow_extend --cov-report=xml \
   --verbose --showlocals --color=yes --durations=30 --basetemp .pytest_basetemp "$@"
 
