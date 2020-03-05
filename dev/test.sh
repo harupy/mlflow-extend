f4bae0dd899bd8ff83a2c78a78fe3f54485f1386
harupy <hkawamura0130@gmail.com>
2020-03-05 22:07:40 +0900

    Fix

--- dev/test.sh
+++ dev/test.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-repot=$([ -z "$GITHUB_ACTION" ] && echo "xml" || echo "term")
+# On GitHub Action, use "xml", "term" otherwise.
+report=$([ ! -z "$GITHUB_ACTION" ] && echo "xml" || echo "term")
 
-pytest tests --doctest-modules mlflow_extend --cov mlflow_extend --cov-report=xml \
+pytest tests --doctest-modules mlflow_extend --cov mlflow_extend --cov-report="$report" \
   --verbose --showlocals --color=yes --durations=30 --basetemp .pytest_basetemp "$@"
-
-
