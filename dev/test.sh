1f6763d465fa62f88f58097c30ded301952a21de
harupy <hkawamura0130@gmail.com>
2020-03-05 21:29:15 +0900

    Add a job for codecov

--- dev/test.sh
+++ dev/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pytest tests --doctest-modules mlflow_extend --cov mlflow_extend \
+pytest tests --doctest-modules mlflow_extend --cov mlflow_extend --cov-report=xml \
   --verbose --showlocals --color=yes --durations=30 --basetemp .pytest_basetemp "$@"
 
 
