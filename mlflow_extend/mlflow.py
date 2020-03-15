35fb32890a58e6812d2fa3e7df2146c4f728c9c0
harupy <hkawamura0130@gmail.com>
2020-03-15 22:41:01 +0900

    Fix mlflow fluent api issue

--- mlflow_extend/mlflow.py
+++ mlflow_extend/mlflow.py
@@ -1,12 +1,3 @@
-# Some fluent APIs are not included in mlflow.__all__:
-# https://github.com/mlflow/mlflow/pull/2511
 from mlflow import *
-from mlflow import (
-    get_experiment,
-    get_experiment_by_name,
-    get_run,
-    get_tracking_uri,
-)
-
 from mlflow_extend.experiment import *
 from mlflow_extend.logging import *
