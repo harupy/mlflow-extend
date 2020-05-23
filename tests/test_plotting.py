2281f8aea9fe105e4c4078ae3a8bc98eedc75295
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-05-24 03:17:05 +0900

    Annotate tests (#79)
    * Add __init__.py
    
    * Add type annotation to test functions
    
    * isort

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,16 +1,20 @@
+from typing import Any
+
 import numpy as np
+import py
 import pytest
 from matplotlib import pyplot as plt
 
 from mlflow_extend import plotting as mplt
+from mlflow_extend.typing import ArrayLike
 
 
-def assert_is_figure(obj):
+def assert_is_figure(obj: Any) -> None:
     assert isinstance(obj, plt.Figure)
 
 
 @pytest.mark.parametrize("cm", [[[1, 2], [3, 4]], np.array([[1, 2], [3, 4]])])
-def test_confusion_matrix(tmpdir, cm):
+def test_confusion_matrix(tmpdir: py.path.local, cm: ArrayLike) -> None:
     fig = mplt.confusion_matrix(cm)
     assert_is_figure(fig)
 
@@ -18,12 +22,12 @@ def test_confusion_matrix(tmpdir, cm):
 @pytest.mark.parametrize(
     "corr", [[[1.0, 2.0], [3.0, 4.0]], np.array([[1.0, 2.0], [3.0, 4.0]])]
 )
-def test_corr_matrix(tmpdir, corr):
+def test_corr_matrix(tmpdir: py.path.local, corr: ArrayLike) -> None:
     fig = mplt.corr_matrix(corr)
     assert_is_figure(fig)
 
 
-def test_feature_importance(tmpdir):
+def test_feature_importance(tmpdir: py.path.local) -> None:
     features = ["a", "b", "c"]
     importances = [1, 2, 3]
     importance_type = "gain"
@@ -32,7 +36,7 @@ def test_feature_importance(tmpdir):
 
 
 @pytest.mark.parametrize("limit", [2, 3, 4])
-def test_feature_importance_with_limit(tmpdir, limit):
+def test_feature_importance_with_limit(tmpdir: py.path.local, limit: int) -> None:
     features = ["a", "b", "c"]
     importances = [1, 2, 3]
     importance_type = "gain"
@@ -40,7 +44,7 @@ def test_feature_importance_with_limit(tmpdir, limit):
     assert_is_figure(fig)
 
 
-def test_feature_importance_with_normalize(tmpdir):
+def test_feature_importance_with_normalize(tmpdir: py.path.local) -> None:
     features = ["a", "b", "c"]
     importances = [1, 2, 3]
     importance_type = "gain"
@@ -50,7 +54,7 @@ def test_feature_importance_with_normalize(tmpdir):
     assert_is_figure(fig)
 
 
-def test_roc_curve(tmpdir):
+def test_roc_curve(tmpdir: py.path.local) -> None:
     fig = mplt.roc_curve([1, 2, 3], [1, 2, 3])
     assert_is_figure(fig)
 
@@ -58,7 +62,7 @@ def test_roc_curve(tmpdir):
     assert_is_figure(fig)
 
 
-def test_pr_curve(tmpdir):
+def test_pr_curve(tmpdir: py.path.local) -> None:
     fig = mplt.pr_curve([1, 2, 3], [1, 2, 3])
     assert_is_figure(fig)
 
