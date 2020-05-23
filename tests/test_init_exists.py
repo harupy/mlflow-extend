d94be103b3bf064dcc3b3d1d73bfa14fe685e398
harupy <hkawamura0130@gmail.com>
2020-05-24 03:34:11 +0900

    Add test to verify __init__.py exists

---
+++ tests/test_init_exists.py
@@ -0,0 +1,14 @@
+import os
+
+import pytest
+
+
+@pytest.mark.parametrize("top", ["mlflow_extend", "tests"])
+def test_init_exists(top_dir: str) -> None:
+    #  mypy skips direcotries that do not contain `__init__.py`.
+    # To prevent that, ensure that `__init__.py` exists in all the directories.
+    for root, _, files in os.walk(top_dir):
+        if any(f.endswith(".py") for f in files):
+            assert (
+                "__init__.py" in files
+            ), "`__init__.py` does not exist in `{}`".format(root)
