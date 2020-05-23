62c1ad94814a0828981aa8b51286dc28ace79c53
harupy <hkawamura0130@gmail.com>
2020-05-24 01:49:21 +0900

    update lint script

--- dev/lint.sh
+++ dev/lint.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-exit_code=0
-
 black --check .
-exit_code=$(( $? | $exit_code ))
-
 isort --check-only .
-exit_code=$(( $? | $exit_code ))
-
 flake8 .
-exit_code=$(( $? | $exit_code ))
-
-exit $exit_code
