ca5cacb7eb70e9df7901f7e06e7859952df2fcde
harupy <hkawamura0130@gmail.com>
2020-05-24 11:38:11 +0900

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools import find_packages, setup
 import mlflow_extend
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
+GITHUB_REPO_URL = "https://github.com/harupy/mlflow-extend"
 
 
-# Use README.md as a long description.
-def get_long_description() -> str:
+def get_readme() -> str:
     with open(os.path.join(ROOT, "README.md"), encoding="utf-8") as f:
         return f.read()
 
@@ -31,14 +31,14 @@ setup(
     install_requires=get_install_requires(),
     maintainer="harupy",
     maintainer_email="hkawamura0130@gmail.com",
-    url="https://github.com/harupy/mlflow-extend",
+    url=GITHUB_REPO_URL,
     project_urls={
         "Documentation": "https://mlflow-extend.readthedocs.io/en/latest/index.html",
-        "Source Code": "https://github.com/harupy/mlflow-extend",
-        "Bug Tracker": "https://github.com/harupy/mlflow-extend/issues",
+        "Source Code": GITHUB_REPO_URL,
+        "Bug Tracker": os.path.join(GITHUB_REPO_URL, "issues"),
     },
     description="Extend MLflow's functionality",
-    long_description=get_long_description(),
+    long_description=get_readme(),
     long_description_content_type="text/markdown",
     classifiers=[
         "License :: OSI Approved :: MIT License",
