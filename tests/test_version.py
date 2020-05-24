cbea04cdcb68861ac73569b30867e1d5514e151f
harupy <hkawamura0130@gmail.com>
2020-05-24 12:02:57 +0900

    add type annotations

--- tests/test_version.py
+++ tests/test_version.py
@@ -3,9 +3,9 @@ import re
 import mlflow_extend
 
 
-def test_version_exists():
+def test_version_exists() -> None:
     assert hasattr(mlflow_extend, "__version__")
 
 
-def test_version_format_is_valid():
+def test_version_format_is_valid() -> None:
     assert re.search(r"^\d+\.\d+\.\d+$", mlflow_extend.__version__) is not None
