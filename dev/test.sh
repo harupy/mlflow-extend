34d4a8dc3db5dc1159c342c35fcbedb0f5a8d418
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-06-22 20:54:05 +0900

    Split pytest command (#108)
    * Split pytest command
    
    * Fix cov param
    
    * fix

--- dev/test.sh
+++ dev/test.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-# On GitHub Action, use "xml", "term" otherwise.
-report=$([ ! -z "$GITHUB_ACTION" ] && echo "xml" || echo "term")
+# On GitHub Action, use "xml" otherwise "term" .
+cov_report_type=$([ ! -z "$GITHUB_ACTION" ] && echo "xml" || echo "term")
 
-pytest tests --doctest-modules mlflow_extend --cov mlflow_extend --cov-report="$report" \
-  --verbose --showlocals --color=yes --durations=30 --basetemp .pytest_basetemp "$@"
+pytest tests \
+  --doctest-modules mlflow_extend \
+  --cov mlflow_extend \
+  --cov-report="$cov_report_type" \
+  --verbose \
+  --showlocals \
+  --color=yes \
+  --durations=30 \
+  --basetemp .pytest_basetemp \
+  "$@"
