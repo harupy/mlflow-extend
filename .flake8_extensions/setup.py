1c1407ac187f664c4f2f140fa15ae1d16decea94
harupy <hkawamura0130@gmail.com>
2020-06-22 19:20:46 +0900

    Add flake8 extension to detect bare pytest.raises

---
+++ .flake8_extensions/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+from version import __version__
+import bare_pytest_raises
+
+setup(
+    name="flake8-extensions",
+    version=__version__,
+    zip_safe=False,
+    py_modules=["bare_pytest_raises"],
+    install_requires=["flake8"],
+    entry_points={
+        "flake8.extension": [
+            f"{bare_pytest_raises.ERROR_CODE_LETTER} = bare_pytest_raises:BarePytestRaises"
+        ]
+    },
+)
