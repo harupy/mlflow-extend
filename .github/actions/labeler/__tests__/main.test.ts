1381fe0c3d872ecca47bfbf850412e033c269861
harupy <hkawamura0130@gmail.com>
2020-06-25 04:36:03 +0900

    update

--- .github/actions/labeler/__tests__/main.test.ts
+++ .github/actions/labeler/__tests__/main.test.ts
@@ -1,7 +1,23 @@
-import { sample } from '../src/main';
+import { extractLabels } from '../src/main';
 
 describe('main', (): void => {
-  it('sample', (): void => {
-    expect(sample()).toEqual('sample');
+  it('extractLabels', (): void => {
+    const body = [
+      '- [] `a`: a',
+      '- [ ] `b`: b',
+      '- [x] `c`: c',
+      '- [ x] `d`: d]',
+      '- [X] `e`: e]',
+      '- [ X] `f`: f]',
+    ].join('\n');
+
+    expect(extractLabels(body)).toEqual([
+      { name: 'a', checked: false },
+      { name: 'b', checked: false },
+      { name: 'c', checked: true },
+      { name: 'd', checked: true },
+      { name: 'e', checked: true },
+      { name: 'f', checked: true },
+    ]);
   });
 });
--- .github/actions/labeler/src/main.ts
+++ .github/actions/labeler/src/main.ts
@@ -1,24 +1,91 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import * as types from '@octokit/types';
 
-export function sample(): string {
-  return 'sample';
+type Label = {
+  name: string;
+  checked: boolean;
+};
+
+export function extractLabels(body: string): Label[] {
+  function helper(regex: RegExp, labels: Label[] = []): Label[] {
+    const res = regex.exec(body);
+    if (res) {
+      const checked = /[xX]/.test(res[1].trim());
+      const name = res[2].trim();
+      return helper(regex, [...labels, { name, checked }]);
+    }
+    return labels;
+  }
+  return helper(/- \[([ xX]*)\] ?`(.+)`/gm);
 }
 
 async function run(): Promise<void> {
   try {
-    // const token: string = core.getInput('github-token');
     const token = core.getInput('repo-token', { required: true });
     const octokit = github.getOctokit(token);
 
+    const { repo, owner } = github.context.repo;
+    const { number: issue_number } = github.context.issue;
+
     const options = octokit.pulls.list.endpoint.merge({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
+      owner,
+      repo,
     });
 
-    for await (const pageResponse of octokit.paginate.iterator(options)) {
-      for (const pullResponse of pageResponse.data) {
-        console.log(pullResponse);
+    for await (const page of octokit.paginate.iterator(options)) {
+      for (const pull of page.data) {
+        // Labels attached on the PR
+        const labelsOnIssueResp = await octokit.issues.listLabelsOnIssue({
+          owner,
+          repo,
+          issue_number,
+        });
+        const labelsOnIssue = labelsOnIssueResp.data.map(({ name }) => name);
+
+        // Labels registered in the repository
+        const labelsForRepoResp = await octokit.issues.listLabelsForRepo({
+          owner,
+          repo,
+        });
+        const labelsForRepo = labelsForRepoResp.data.map(({ name }) => name);
+
+        // Labels in the PR description
+        const { body } = pull as types.PullsGetResponseData;
+        const labels = extractLabels(body).filter(({ name }) =>
+          labelsForRepo.includes(name),
+        );
+
+        // Remove unchecked labels
+        const labelsToRemove = labels.filter(
+          ({ name, checked }) => !checked && labelsOnIssue.includes(name),
+        );
+        labelsToRemove.forEach(async ({ name }) => {
+          await octokit.issues.removeLabel({
+            owner,
+            repo,
+            issue_number,
+            name,
+          });
+        });
+
+        // Add checked labels
+        const labelsToAdd = labels
+          .filter(
+            ({ name, checked }) => checked && !labelsOnIssue.includes(name),
+          )
+          .map(({ name }) => name);
+
+        if (labelsToAdd.length > 0) {
+          await octokit.issues.addLabels({
+            owner,
+            repo,
+            issue_number,
+            labels: labelsToAdd,
+          });
+        }
+
+        console.log(`issue_number: ${issue_number}`);
       }
     }
   } catch (error) {
