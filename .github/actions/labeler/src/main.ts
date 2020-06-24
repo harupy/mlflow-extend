1a6e123cce473f1d5f57445bfdc493b4ef455cf0
Harutaka Kawamura <hkawamura0130@gmail.com>
2020-06-25 05:24:44 +0900

    Add labeler (#112)
    * Add labeler
    
    * Fix working directory
    
    * Add main
    
    * Add build
    
    * exit if git diff is not empty
    
    * Add actions/github
    
    * Add octokit
    
    * Add test-run
    
    * Fix path
    
    * fix input name
    
    * Fix action path again
    
    * Add checkout
    
    * Add index.js
    
    * update git diff logic
    
    * fix
    
    * Remove ()
    
    * Add name for clarification
    
    * update dist/index.js
    
    * Use caller repo info
    
    * debug
    
    * fix
    
    * update
    
    * Add octokit/types
    
    * change job name
    
    * update
    
    * pack
    
    * udpate
    
    * run and pack
    
    * rename
    
    * run and pack
    
    * debug
    
    * rerun
    
    * remove console.log
    
    * debug
    
    * fix
    
    * Add cron action
    
    * remove checkout
    
    * fix cron string
    
    * remove needs
    
    * remove types/node

---
+++ .github/actions/labeler/src/main.ts
@@ -0,0 +1,100 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as types from '@octokit/types';
+
+type Label = {
+  name: string;
+  checked: boolean;
+};
+
+export function extractLabels(body: string): Label[] {
+  function helper(regex: RegExp, labels: Label[] = []): Label[] {
+    const res = regex.exec(body);
+    if (res) {
+      const checked = /[xX]/.test(res[1].trim());
+      const name = res[2].trim();
+      return helper(regex, [...labels, { name, checked }]);
+    }
+    return labels;
+  }
+  return helper(/- \[([ xX]*)\] ?`(.+)`/gm);
+}
+
+async function main(): Promise<void> {
+  try {
+    const token = core.getInput('repo-token', { required: true });
+    const octokit = github.getOctokit(token);
+
+    const { repo, owner } = github.context.repo;
+    const options = octokit.pulls.list.endpoint.merge({
+      owner,
+      repo,
+    });
+
+    // Iterate over all the open PRs
+    for await (const page of octokit.paginate.iterator(options)) {
+      for (const pull of page.data) {
+        const {
+          body,
+          number: issue_number,
+        } = pull as types.PullsGetResponseData;
+
+        // Labels attached on the PR
+        const labelsOnIssueResp = await octokit.issues.listLabelsOnIssue({
+          owner,
+          repo,
+          issue_number,
+        });
+        const labelsOnIssue = labelsOnIssueResp.data.map(({ name }) => name);
+
+        // Labels registered in the repository
+        const labelsForRepoResp = await octokit.issues.listLabelsForRepo({
+          owner,
+          repo,
+        });
+        const labelsForRepo = labelsForRepoResp.data.map(({ name }) => name);
+
+        // Labels in the PR description
+        const labels = extractLabels(body).filter(({ name }) =>
+          // Remove labels that are not registered in the repo.
+          labelsForRepo.includes(name),
+        );
+
+        // Remove unchecked labels
+        const labelsToRemove = labels.filter(
+          ({ name, checked }) => !checked && labelsOnIssue.includes(name),
+        );
+        labelsToRemove.forEach(async ({ name }) => {
+          await octokit.issues.removeLabel({
+            owner,
+            repo,
+            issue_number,
+            name,
+          });
+        });
+
+        // Add checked labels
+        const labelsToAdd = labels
+          .filter(
+            ({ name, checked }) => checked && !labelsOnIssue.includes(name),
+          )
+          .map(({ name }) => name);
+
+        if (labelsToAdd.length > 0) {
+          await octokit.issues.addLabels({
+            owner,
+            repo,
+            issue_number,
+            labels: labelsToAdd,
+          });
+        }
+
+        console.log(`issue_number: ${issue_number}`);
+      }
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
